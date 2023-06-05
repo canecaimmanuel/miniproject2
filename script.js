3e0890e4dc73d5572467212fc3c042a4a586ed96
CarloMiranda <carlomiranda161@gmail.com>
2023-06-06 02:54:58 +0800

    adding onscroll Up-arrow in all pages

--- script.js
+++ script.js
@@ -57,4 +57,18 @@ document.addEventListener("DOMContentLoaded", function (){
     animateLoop();
 });
 
+window.addEventListener('scroll', function() {
+    var scrollBtn = document.getElementById('scroll-up-btn');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollBtn.style.display = 'block';
+    } else {
+      scrollBtn.style.display = 'none';
+    }
+  });
+  
+  document.getElementById('scroll-up-btn').addEventListener('click', function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+
 
