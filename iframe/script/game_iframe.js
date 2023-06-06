574a7bee8318195be532536a01433b8df50f6f0e
joelo23 <laberintoj8@gmail.com>
2023-06-06 23:26:57 +0800

    CHANGING LOCATION

--- iframe/script/game_iframe.js
+++ iframe/script/game_iframe.js
@@ -6,9 +6,6 @@ var loc2 = "game.html"
 
 iframe.src = loc1 + loc2;
 
-
-iframe.width = '100%'; // Set width to 100%
-iframe.height = '300';
 iframe.frameBorder = '0';
 
 var container = document.getElementById('canvas');
